b0bc845bc43919c65bea7c2596ff628ec96a65cc
Elvira Semenova <shalopayka@gmail.com>
2020-11-17 22:47:54 +0300

    data type

---
+++ src/ru/job4j/PrimitiveDataType.java
@@ -0,0 +1,14 @@
+package ru.job4j;
+
+public class PrimitiveDataType {
+    public static void main(String[] args) {
+        int age = 18; //возраст
+        char sex = 'W'; //пол
+        int height = 180; //рост
+        int numberOfBricks = 25000; //количество кирпичей
+        long numberOfBacteria = 100000000000000L; //количество бактерий
+        long numberOfKilometers = 150000000; //расстояние до космических объектов
+        boolean permition = true; //разрешение на работу
+        int volumeMegabyte = 1000; //размер файла
+    }
+}
