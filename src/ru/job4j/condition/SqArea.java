44551d487f20dc9d8e1ec5866ce3bed5954b3ad6
Elvira Semenova <shalopayka@gmail.com>
2020-11-19 14:25:15 +0300

    commit666

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -2,13 +2,12 @@ package ru.job4j.condition;
 
 public class SqArea {
     public static double square(int p, int k) {
-
-        return ((p / (2 * (k + 1))) * k) * (p / (2 * (k + 1)));
-
+        double rsl = -1;
+        return rsl;
     }
 
     public static void main(String[] args) {
-        double result1 = SqArea.square(6, 2);
-        System.out.println(" p = 6, k = 2, s = 2, real = " + result1);
+        double result1 = SqArea.square(4, 1);
+        System.out.println(" p = 4, k = 1, s = 1, real = " + result1);
     }
 }
