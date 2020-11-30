285fa47f6f946e36bb883577398b54a72edbc588
Elvira Semenova <shalopayka@gmail.com>
2020-11-30 16:24:53 +0300

    Merge

--- src/ru/job4j/array/Merge.java
+++ src/ru/job4j/array/Merge.java
@@ -3,12 +3,24 @@ package ru.job4j.array;
 public class Merge {
     public static int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
-        for (int i = 0; i < left.length; i++) {
-            for (int j = 0; j < right.length; j++) {
-                rsl[i] = left[i] + right[j];
+        for (int i = 0, l = 0, r = 0; i < rsl.length; i++) {
+            if (l == left.length) {
+                rsl[i] = right[r];
+                r++;
+                continue;
+            } else if (r == right.length) {
+                rsl[i] = left[l];
+                l++;
+                continue;
+            }
+            if (left[l] <= right[r]) {
+                rsl[i] = left[l];
+                l++;
+            } else {
+                rsl[i] = right[r];
+                r++;
             }
         }
         return rsl;
     }
 }
-
